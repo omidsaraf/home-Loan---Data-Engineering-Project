87838c17ca91f1f5c5516358d5e667b2f76a34b1
Omid Saraf <omidsarafmanesh.osm@outlook.com>
2025-07-20 15:20:48 +1000

    Delete Extraction/Application/api_utils.py

--- Extraction/Application/api_utils.py
+++
@@ -1,9 +0,0 @@
-from pyspark.sql import SparkSession
-
-def get_spark_session():
-    return SparkSession.builder \
-        .appName("HomeLoanIQ API") \
-        .getOrCreate()
-
-def load_delta_table(spark, table_path):
-    return spark.read.format("delta").load(table_path)
